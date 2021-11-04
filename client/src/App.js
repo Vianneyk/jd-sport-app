3c4537bc68dbb03293f1294374a1cb92aaa17b8e
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-11-04 14:21:44 +0000

    added img and div styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./index.css";
+// import Products from "./components/Products";
+
+// function App() {
+// 	return (
+// 		<div className="product-container">
+// 			<Products />
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from "react";
+import Product from "./components/Products";
+
+const API =
+	"https://size-client-resources.s3.amazonaws.com/Dev-Task/FE-Dev-Task-Data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		fetch(API)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				setProducts(data.dataExample);
+			});
+	}, []);
+
+	return (
+		<div className="product-container">
+			{products.length > 0 &&
+				products.map(product => <Product key={product.id} {...product} />)}
+		</div>
+	);
 }
 
 export default App;
